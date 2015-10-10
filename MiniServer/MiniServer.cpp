7484887e1e140c3e8dab7a7f61919094e80cc1a7
wp4398151 <wp4398151@qq.com>
2015-10-10 13:41:10 +0800

    增加glog

--- MiniServer/MiniServer.cpp
+++ MiniServer/MiniServer.cpp
@@ -2,10 +2,13 @@
 //
 
 #include "stdafx.h"
+//#include "WpLogHelper.h"
 
-#include "WpLogHelper.h"
+#include <glog/logging.h>
+#include <windows.h>
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	LOG(INFO) << "GLOG !";
 	return 0;
 }
